06346ba3ea7f8f9f7dcb72dfabb30f5efd680c31
Hamish <LucidNeko@users.noreply.github.com>
2014-10-20 20:28:39 +1300

    Added code to Collider so we can have Colliders that are triggers.

--- src/engine/components/Collider.java
+++ src/engine/components/Collider.java
@@ -9,10 +9,25 @@ import engine.core.Component;
  */
 public abstract class Collider extends Component {
 
+	private boolean isTrigger = false;
+
 	/**
 	 * Gets the smallest AABB that fully encloses this Collider
 	 * @return The AABB.
 	 */
 	public abstract AABB getAABB();
+	
+	/**
+	 * Sets this colliders trigger state.
+	 * @param isTrigger <br>If true -> isTrigger() will return true. <br>If false -> isTrigger() will return false.
+	 */
+	public void setTrigger(boolean isTrigger) {
+		this.isTrigger = isTrigger;
+	}
+	
+	/** Returns true if this Collider is a trigger. */
+	public boolean isTrigger() {
+		return isTrigger;
+	}
 	
 }
