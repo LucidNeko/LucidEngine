8f6831ccc80942e23fed9a0091f4df8c5f877238
Hamish <LucidNeko@users.noreply.github.com>
2014-10-22 00:48:12 +1300

    Extracted out player movement into anonymous Behaviour.

--- src/cub3d/Game.java
+++ src/cub3d/Game.java
@@ -13,6 +13,7 @@ import cub3d.assets.Resources;
 import engine.common.Mathf;
 import engine.common.Vec3;
 import engine.components.Behaviour;
+import engine.components.Transform;
 import engine.components.Transform.Space;
 import engine.core.Entity;
 import engine.core.GameLoop;
@@ -60,22 +61,42 @@ public class Game extends GameLoop {
 	}
 	
 	private void setup() throws IOException {
-		Mesh mesh = new OBJBuilder(Resources.getInputStream("link.obj")).getMesh();
-		Material material = new Material(new Texture(Resources.getImage("link.png"), true), 1, 1, 1, 1);
+		Mesh mesh;
+		Material material;
+		
+//		mesh = new OBJBuilder(Resources.getInputStream("teddy.obj")).getMesh().getScaledInstance(0.25f);
+//		material = new Material(new Texture(Resources.getImage("teddy.png"), true), 1, 1, 1, 1);
+		
+		mesh = new OBJBuilder(Resources.getInputStream("link.obj")).getMesh();
+		material = new Material(new Texture(Resources.getImage("link.png"), true), 1, 1, 1, 1);
 		
 		Entity link = world.createEntity("Link");
 		link.attachComponent(MeshFilter.class).setMesh(mesh);
 		link.attachComponent(MeshRenderer.class).setMaterial(material);
-		
-//		mesh = new OBJBuilder(Resources.getInputStream("teddy.obj")).getMesh().getScaledInstance(0.25f);
-//		material = new Material(new Texture(Resources.getImage("teddy.png"), true), 1, 1, 1, 1);
+		link.attachComponent(new Behaviour() {
+			@Override
+			public void update(float delta) {
+				Transform transform = getOwner().getTransform();
+				if(Keyboard.isKeyDown(KeyEvent.VK_W))
+					transform.translate(0, 0, 1*delta, Space.LOCAL);
+				if(Keyboard.isKeyDown(KeyEvent.VK_A))
+					transform.rotate(Mathf.degToRad(360*delta), Vec3.UP(), Space.LOCAL);
+				if(Keyboard.isKeyDown(KeyEvent.VK_S))
+					transform.translate(0, 0, -1*delta, Space.LOCAL);
+				if(Keyboard.isKeyDown(KeyEvent.VK_D))
+					transform.rotate(-Mathf.degToRad(360*delta), Vec3.UP(), Space.WORLD);		
+				if(Keyboard.isKeyDown(KeyEvent.VK_SPACE))
+					transform.rotate(Mathf.degToRad(360*delta), Vec3.RIGHT(), Space.LOCAL);
+			}
+		});
+
 		Entity last = link;
 		for(int i = 0; i < 20; i++) {
 			Entity teddy = world.createEntity("Teddy" + i);
 			teddy.attachComponent(MeshFilter.class).setMesh(mesh);
 			teddy.attachComponent(MeshRenderer.class).setMaterial(material);
 			teddy.getTransform().setParent(last.getTransform());
-			teddy.getTransform().translate(0, Mathf.sin(i), -0.1f*(i+1), Space.LOCAL);
+			teddy.getTransform().translate(0, Mathf.sin(i), -0.1f*(i+5), Space.LOCAL);
 			teddy.attachComponent(new Behaviour() {
 				@Override
 				public void update(float delta) {
@@ -98,19 +119,6 @@ public class Game extends GameLoop {
 		for(Entity e : world.getEntities())
 			for(Behaviour b : e.getComponents(Behaviour.class))
 				b.update(delta);
-		
-		Entity link = world.getEntity("Link");
-		
-		if(Keyboard.isKeyDown(KeyEvent.VK_W))
-			link.getTransform().translate(0, 0, 1*delta, Space.LOCAL);
-		if(Keyboard.isKeyDown(KeyEvent.VK_A))
-			link.getTransform().rotate(Mathf.degToRad(360*delta), Vec3.UP(), Space.LOCAL);
-		if(Keyboard.isKeyDown(KeyEvent.VK_S))
-			link.getTransform().translate(0, 0, -1*delta, Space.LOCAL);
-		if(Keyboard.isKeyDown(KeyEvent.VK_D))
-			link.getTransform().rotate(-Mathf.degToRad(360*delta), Vec3.UP(), Space.WORLD);		
-		if(Keyboard.isKeyDown(KeyEvent.VK_SPACE))
-			link.getTransform().rotate(Mathf.degToRad(360*delta), Vec3.RIGHT(), Space.LOCAL);
 	}
 
 	@Override
