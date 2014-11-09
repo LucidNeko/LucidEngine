4a8e3e10430c77e88e56bb56582de06cc0ddefc2
Hamish <LucidNeko@users.noreply.github.com>
2014-11-10 11:29:51 +1300

    Added more test code to Game.

--- src/cub3d/Game.java
+++ src/cub3d/Game.java
@@ -13,6 +13,7 @@ import engine.common.Vec3;
 import engine.components.Behaviour;
 import engine.components.Transform;
 import engine.components.Transform.Space;
+import engine.core.Camera;
 import engine.core.Display;
 import engine.core.Engine;
 import engine.core.Entity;
@@ -43,6 +44,8 @@ public class Game {
 			e.printStackTrace();
 		}
 		
+		Mouse.setGrabbed(true);
+		
 		Engine.start();
 	}
 	
@@ -56,10 +59,7 @@ public class Game {
 		mesh = new OBJBuilder(Resources.getInputStream("link.obj")).getMesh();
 		material = new Material(new Texture(Resources.getImage("link.png"), true), 1, 1, 1, 1);
 		
-		Entity link = World.createEntity("Link");
-		link.getTransform().translate(0, 0, -10, Space.WORLD);
-		link.attachComponent(MeshFilter.class).setMesh(mesh);
-		link.attachComponent(MeshRenderer.class).setMaterial(material);
+		Entity link = World.createEntity("Player");
 		link.attachComponent(new Behaviour() {
 
 			@Override
@@ -70,62 +70,103 @@ public class Game {
 			
 			@Override
 			public void update() {
+
+				if(Keyboard.isKeyDown(KeyEvent.VK_ESCAPE))
+					System.exit(0);
+				
 				float delta = Time.getDeltaTime();
-				log.trace("time={}", delta);
 				Transform transform = getOwner().getTransform();
+				
+				float mdx = Mouse.getDX();
+				float mdy = Mouse.getDY();
+				
+				if(mdx != 0) {
+					transform.rotate(-mdx*delta, Vec3.UP(), Space.WORLD);
+				}
+				
+				if(mdy != 0) {
+					transform.rotate(-mdy*delta, Vec3.RIGHT(), Space.LOCAL);
+				}
+				
 				if(Keyboard.isKeyDown(KeyEvent.VK_W))
-					transform.translate(0, 0, 1*delta, Space.LOCAL);
+					transform.translate(0, 0, 5*delta, Space.LOCAL);
 				if(Keyboard.isKeyDown(KeyEvent.VK_A))
-					transform.rotate(Mathf.degToRad(360*delta), Vec3.UP(), Space.LOCAL);
+					transform.translate(5*delta, 0, 0, Space.LOCAL);
 				if(Keyboard.isKeyDown(KeyEvent.VK_S))
-					transform.translate(0, 0, -1*delta, Space.LOCAL);
+					transform.translate(0, 0, -5*delta, Space.LOCAL);
 				if(Keyboard.isKeyDown(KeyEvent.VK_D))
-					transform.rotate(-Mathf.degToRad(360*delta), Vec3.UP(), Space.WORLD);		
-				if(Keyboard.isKeyDown(KeyEvent.VK_SPACE))
-					transform.rotate(Mathf.degToRad(360*delta), Vec3.RIGHT(), Space.LOCAL);
+					transform.translate(-5*delta, 0, 0, Space.LOCAL);	
 			}
 			
 			public void fixedUpdate() {
-				log.trace("fixedTime={}", Time.getFixedDeltaTime());
+				
+				
 			}
 		});
+		
+//		Camera.getInstance().getTransform().translate(0, 0, -1, Space.LOCAL);
+		Camera.getInstance().getTransform().setParent(link.getTransform());
 
 		final Entity flink = link;
 		final Mesh fmesh = mesh;
 		final Material fmet = material;
-		final Task task = new Task() {
-			
-			float time = 0;
-			
-			Entity last = flink;
+		
+		TaskManager.addTask(new Task() {
 
+			float time = 1f;
+			
 			@Override
 			public boolean isFinished() {
-				return time > 1;
+				return time <= 0;
 			}
 
 			@Override
 			public Object execute() {
-				time += Time.getDeltaTime();
+				time -= Time.getDeltaTime();
 				
-				Entity teddy = World.createEntity("Teddy" + time);
+				final Entity teddy = World.createEntity("Teddy" + time);
 				teddy.attachComponent(MeshFilter.class).setMesh(fmesh);
 				teddy.attachComponent(MeshRenderer.class).setMaterial(fmet);
-				teddy.getTransform().setParent(last.getTransform());
-				teddy.getTransform().translate(0, Mathf.sin(1*time), 1*time, Space.LOCAL);
-				teddy.attachComponent(new Behaviour() { 
-					public void start() { } 
-					public void update() {
-						getOwner().getTransform().rotate(Mathf.degToRad(10*Time.getDeltaTime()), Vec3.UP(), Space.LOCAL);
-					} 
-				});
-				last = teddy;
+				teddy.getTransform().translate(-20+Mathf.random()*40, 0, -20+Mathf.random()*40, Space.LOCAL);
+				teddy.getTransform().rotate(Mathf.PI*2*Mathf.random(), Vec3.UP(), Space.LOCAL);
 
+				TaskManager.addTask(new Task() {
+					
+					float time = 0;
+					
+					Entity last = teddy;
+
+					@Override
+					public boolean isFinished() {
+						return time > 3.5f;
+					}
+
+					@Override
+					public Object execute() {
+						time += Time.getDeltaTime();
+						
+						Entity teddy = World.createEntity("Teddy" + time);
+						teddy.attachComponent(MeshFilter.class).setMesh(fmesh);
+						teddy.attachComponent(MeshRenderer.class).setMaterial(fmet);
+						teddy.getTransform().setParent(last.getTransform());
+						teddy.getTransform().translate(0, Mathf.sin(1*time), 1*time, Space.LOCAL);
+						teddy.attachComponent(new Behaviour() { 
+							public void start() { } 
+							public void update() {
+								getOwner().getTransform().rotate(Mathf.degToRad(10*Time.getDeltaTime()), Vec3.UP(), Space.LOCAL);
+							} 
+						});
+						last = teddy;
+
+						return null;
+					}
+					
+				});
+				
 				return null;
 			}
 			
-		};
-		TaskManager.addTask(task);
+		});
 		
 	}
 	
