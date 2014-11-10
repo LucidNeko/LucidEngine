0440567fa0e615e9765f10db9d2a67c5b5184a1b
Hamish <LucidNeko@users.noreply.github.com>
2014-11-10 14:33:53 +1300

    Playing around with Cloth.

--- src/cub3d/Cloth.java
+++ src/cub3d/Cloth.java
@@ -1,30 +1,264 @@
 package cub3d;
 
+import java.awt.event.KeyEvent;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.media.opengl.GL2;
+
+import engine.common.Vec3;
 import engine.components.Behaviour;
+import engine.core.Time;
+import engine.input.Keyboard;
+import engine.opengl.GL2Renderer;
 
 public class Cloth extends Behaviour {
-	
-	public float width;
-	public float height;
-	
-	private float _width;
-	private float _height;
-	
+
+	public int _width;
+	public int _height;
+	public int _precision = 1;
+
+	private int width;
+	private int height;
+	private int precision;
+
+	private List<Particle> particles = new LinkedList<Particle>();
+	private List<Constraint> constraints = new LinkedList<Constraint>();
+
 
 	@Override
 	public void start() {
-		setSize(width, height);
+		setSize(_width, _height);
+		precision = _precision;
+		reset();
+
+//		getOwner().attachComponent(new GL2Renderer() {
+//
+//			@Override
+//			public void render(GL2 gl) {
+//				gl.glDisable(GL2.GL_LIGHTING);
+//				gl.glColor3f(0, 0, 1);
+//				gl.glPointSize(5);
+//				gl.glBegin(GL2.GL_POINTS);
+//					for(Particle p : particles) {
+//						gl.glVertex3f(p.pos.x(), p.pos.y(), p.pos.z());
+//					}
+//				gl.glEnd();
+//
+//
+//					for(Constraint c : constraints) {
+//						if(c instanceof DistanceConstraint) {
+//							gl.glColor3f(0, 1, 0);
+//							gl.glBegin(GL2.GL_LINES);
+//								DistanceConstraint dc = (DistanceConstraint)c;
+//								gl.glVertex3f(dc.a.pos.x(), dc.a.pos.y(), dc.a.pos.z());
+//								gl.glVertex3f(dc.b.pos.x(), dc.b.pos.y(), dc.b.pos.z());
+//							gl.glEnd();
+//						} else if(c instanceof PinConstraint) {
+//							gl.glColor3f(1, 0, 0);
+//							gl.glPointSize(3);
+//							gl.glBegin(GL2.GL_POINTS);
+//								PinConstraint pc = (PinConstraint)c;
+//								gl.glVertex3f(pc.a.pos.x(), pc.a.pos.y(), pc.a.pos.z());
+//							gl.glEnd();
+//						}
+//					}
+//					gl.glEnable(GL2.GL_LIGHTING);
+//			}
+//
+//		});
 	}
-	
-	public void setSize(float width, float height) {
-		this._width = width;
-		this._height = height;
+
+	public void setSize(int width, int height) {
+		this.width = width;
+		this.height = height;
 	}
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
-		
+		integrate(Time.getDeltaTime()*Time.getDeltaTime());
+		solve();
+	}
+
+	private void reset() {
+		particles.clear();
+		constraints.clear();
+
+//		for(int y = 0; y < height; y++) {
+//			for(int x = 0; x < width; x++) {
+//				float step = 1/precision;
+//				float currentStep = 0;
+//				for(int i = 0; i < precision; i++) {
+//					particles
+//					currentStep += step;
+//				}
+//			}
+//		}
+
+		final Particle[][] ps = new Particle[height*precision][width*precision];
+		for(int row = 0; row < ps.length; row++) {
+			for(int col = 0; col < ps[row].length; col++) {
+				ps[row][col] = new Particle(col*(width/(float)precision), row*(height/(float)precision), 0);
+				particles.add(ps[row][col]);
+			}
+		}
+
+		for(int row = 0; row < ps.length; row++) {
+			for(int col = 0; col < ps[row].length; col++) {
+				if(col+1 < ps[row].length)
+					constraints.add(new DistanceConstraint(ps[row][col], ps[row][col+1]));
+				if(row+1 < ps.length)
+					constraints.add(new DistanceConstraint(ps[row][col], ps[row+1][col]));
+			}
+		}
+
+		for(int col = 0; col < ps[0].length; col++) {
+			constraints.add(new PinConstraint(ps[0][col]));
+		}
+
+		getOwner().attachComponent(new GL2Renderer() {
+
+
+
+			@Override
+			public void render(GL2 gl) {
+				gl.glColor3f(0, 0, 0.8f);
+				for(int row = 0; row < ps.length; row++) {
+					for(int col = 0; col < ps[row].length; col++) {
+						try {
+							Vec3 a = ps[row][col].pos;
+							Vec3 b = ps[row+1][col].pos;
+							Vec3 c = ps[row+1][col+1].pos;
+							Vec3 d = ps[row][col+1].pos;
+
+							//tri 1
+							{
+								Vec3 normal = (b.sub(a)).cross(d.sub(a));
+								normal.normalize();
+								gl.glBegin(GL2.GL_TRIANGLES);
+									gl.glVertex3f(a.x(), a.y(), a.z());
+									gl.glNormal3f(normal.x(), normal.y(), normal.z());
+									gl.glVertex3f(b.x(), b.y(), b.z());
+									gl.glNormal3f(normal.x(), normal.y(), normal.z());
+									gl.glVertex3f(d.x(), d.y(), d.z());
+									gl.glNormal3f(normal.x(), normal.y(), normal.z());
+								gl.glEnd();
+							}
+
+							//tri 2
+							{
+								Vec3 normal = (b.sub(d)).cross(c.sub(d));
+								normal.normalize();
+								gl.glBegin(GL2.GL_TRIANGLES);
+									gl.glVertex3f(d.x(), d.y(), d.z());
+									gl.glNormal3f(normal.x(), normal.y(), normal.z());
+									gl.glVertex3f(b.x(), b.y(), b.z());
+									gl.glNormal3f(normal.x(), normal.y(), normal.z());
+									gl.glVertex3f(c.x(), c.y(), c.z());
+									gl.glNormal3f(normal.x(), normal.y(), normal.z());
+								gl.glEnd();
+							}
+						} catch(Exception e) {
+
+						}
+					}
+				}
+			}
+
+		});
+
+		getOwner().attachComponent(new Behaviour() {
+
+			@Override
+			public void start() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void update() {
+				if(Keyboard.isKeyDown(KeyEvent.VK_P)) {
+					ps[ps.length-1][ps[0].length-1].pos.addLocal(0, 0, 1*Time.getDeltaTime());
+				}
+			}
+
+		});
+	}
+
+	private void integrate(float deltaSquared) {
+		for(int i = 0; i < 2; i++)
+			for(Particle p : particles)
+				p.integrate(deltaSquared);
+	}
+
+	private void solve() {
+		for(int i = 0; i < 7; i++)
+			for(Constraint c : constraints)
+				c.solve();
+	}
+
+	private class Particle {
+
+		Vec3 oldPos, pos;
+
+		public Particle(float x, float y, float z) {
+			oldPos = new Vec3(x, y, z);
+			pos = new Vec3(x, y, z);
+		}
+
+		public void integrate(float deltaSquared) {
+			float nx = pos.x() + (pos.x() - oldPos.x())*0.98f + 0*deltaSquared;
+			float ny = pos.y() + (pos.y() - oldPos.y())*0.98f - 1*deltaSquared;
+			float nz = pos.z() + (pos.z() - oldPos.z())*0.98f + 0*deltaSquared;
+			oldPos.set(pos);
+			pos.set(nx, ny, nz);
+		}
+
+	}
+
+	private interface Constraint {
+		public void solve();
+	}
+
+	private class DistanceConstraint implements Constraint {
+
+		private Particle a;
+		private Particle b;
+		private float length;
+
+		public DistanceConstraint(Particle a, Particle b) {
+			this.a = a;
+			this.b = b;
+			this.length = a.pos.sub(b.pos).length();
+		}
+
+		public void solve() {
+			Vec3 diff = a.pos.sub(b.pos);
+			float d = diff.length();
+			float difference = (length - d) / d;
+			Vec3 trans = diff.mul(0.5f).mul(difference);
+			a.pos.addLocal(trans);
+			b.pos.subLocal(trans);
+		}
+
+	}
+
+	private class PinConstraint implements Constraint {
+
+		private Particle a;
+		private Vec3 pin;
+
+
+		public PinConstraint(Particle a) {
+			this.a = a;
+			pin = a.pos.clone();
+		}
+
+		@Override
+		public void solve() {
+			a.pos.set(pin);
+		}
+
 	}
 
 }
--- src/cub3d/Game.java
+++ src/cub3d/Game.java
@@ -33,61 +33,68 @@ import engine.util.OBJBuilder;
 
 public class Game {
 	private static final Logger log = LogManager.getLogger();
-	
+
 	public Game() {
 		Display.create(1920, 1080);
-		
+
 		try {
 			setup();
 		} catch (IOException e) {
 			log.error("Failed setting up game");
 			e.printStackTrace();
 		}
-		
+
 		Mouse.setGrabbed(true);
-		
+
 		Engine.start();
 	}
-	
+
 	private void setup() throws IOException {
 		Mesh mesh;
 		Material material;
-		
+
 //		mesh = new OBJBuilder(Resources.getInputStream("teddy.obj")).getMesh().getScaledInstance(0.25f);
 //		material = new Material(new Texture(Resources.getImage("teddy.png"), true), 1, 1, 1, 1);
-		
+
 		mesh = new OBJBuilder(Resources.getInputStream("link.obj")).getMesh();
 		material = new Material(new Texture(Resources.getImage("link.png"), true), 1, 1, 1, 1);
-		
+
+		Entity cloth = World.createEntity("Cloth");
+		Cloth c = cloth.attachComponent(Cloth.class);
+		c._width = 2;
+		c._height = 2;
+		c._precision = 10;
+		c.start();
+
 		Entity link = World.createEntity("Player");
 		link.attachComponent(new Behaviour() {
 
 			@Override
 			public void start() {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void update() {
 
 				if(Keyboard.isKeyDown(KeyEvent.VK_ESCAPE))
 					System.exit(0);
-				
+
 				float delta = Time.getDeltaTime();
 				Transform transform = getOwner().getTransform();
-				
+
 				float mdx = Mouse.getDX();
 				float mdy = Mouse.getDY();
-				
+
 				if(mdx != 0) {
-					transform.rotate(-mdx*delta, Vec3.UP(), Space.WORLD);
+					transform.rotate(-mdx*delta*0.2f, Vec3.UP(), Space.WORLD);
 				}
-				
+
 				if(mdy != 0) {
-					transform.rotate(-mdy*delta, Vec3.RIGHT(), Space.LOCAL);
+					transform.rotate(-mdy*delta*0.2f, Vec3.RIGHT(), Space.LOCAL);
 				}
-				
+
 				if(Keyboard.isKeyDown(KeyEvent.VK_W))
 					transform.translate(0, 0, 5*delta, Space.LOCAL);
 				if(Keyboard.isKeyDown(KeyEvent.VK_A))
@@ -95,81 +102,93 @@ public class Game {
 				if(Keyboard.isKeyDown(KeyEvent.VK_S))
 					transform.translate(0, 0, -5*delta, Space.LOCAL);
 				if(Keyboard.isKeyDown(KeyEvent.VK_D))
-					transform.translate(-5*delta, 0, 0, Space.LOCAL);	
+					transform.translate(-5*delta, 0, 0, Space.LOCAL);
 			}
-			
+
 			public void fixedUpdate() {
-				
-				
+
+
 			}
 		});
-		
+
 //		Camera.getInstance().getTransform().translate(0, 0, -1, Space.LOCAL);
 		Camera.getInstance().getTransform().setParent(link.getTransform());
 
 		final Entity flink = link;
 		final Mesh fmesh = mesh;
 		final Material fmet = material;
-		
-		TaskManager.addTask(new Task() {
 
-			float time = 1f;
-			
-			@Override
-			public boolean isFinished() {
-				return time <= 0;
-			}
+//		TaskManager.addTask(new Task() {
+//
+//			float time = 1f;
+//
+//			@Override
+//			public boolean isFinished() {
+//				return time <= 0;
+//			}
+//
+//			@Override
+//			public Object execute() {
+//				time -= Time.getDeltaTime();
+//
+//				final Entity teddy = World.createEntity("Teddy" + time);
+//				teddy.attachComponent(MeshFilter.class).setMesh(fmesh);
+//				teddy.attachComponent(MeshRenderer.class).setMaterial(fmet);
+//				teddy.getTransform().translate(-20+Mathf.random()*40, 0, -20+Mathf.random()*40, Space.LOCAL);
+//				teddy.getTransform().rotate(Mathf.PI*2*Mathf.random(), Vec3.UP(), Space.LOCAL);
+//
+//				TaskManager.addTask(new Task() {
+//
+//					float time = 0;
+//
+//					Entity last = teddy;
+//
+//					@Override
+//					public boolean isFinished() {
+//						return time > 3.5f;
+//					}
+//
+//					@Override
+//					public Object execute() {
+//						time += Time.getDeltaTime();
+//
+//						Entity teddy = World.createEntity("Teddy" + time);
+//						teddy.attachComponent(MeshFilter.class).setMesh(fmesh);
+//						teddy.attachComponent(MeshRenderer.class).setMaterial(fmet);
+//						teddy.getTransform().setParent(last.getTransform());
+//						teddy.getTransform().translate(0, Mathf.sin(1*time), 1*time, Space.LOCAL);
+//						teddy.attachComponent(new Behaviour() {
+//							public void start() { }
+//							public void update() {
+//								getOwner().getTransform().rotate(Mathf.degToRad(10*Time.getDeltaTime()), Vec3.UP(), Space.LOCAL);
+//							}
+//						});
+//						last = teddy;
+//
+//						return null;
+//					}
+//
+//					@Override
+//					public void remove() {
+//						// TODO Auto-generated method stub
+//
+//					}
+//
+//				});
+//
+//				return null;
+//			}
+//
+//			@Override
+//			public void remove() {
+//				// TODO Auto-generated method stub
+//
+//			}
+//
+//		});
 
-			@Override
-			public Object execute() {
-				time -= Time.getDeltaTime();
-				
-				final Entity teddy = World.createEntity("Teddy" + time);
-				teddy.attachComponent(MeshFilter.class).setMesh(fmesh);
-				teddy.attachComponent(MeshRenderer.class).setMaterial(fmet);
-				teddy.getTransform().translate(-20+Mathf.random()*40, 0, -20+Mathf.random()*40, Space.LOCAL);
-				teddy.getTransform().rotate(Mathf.PI*2*Mathf.random(), Vec3.UP(), Space.LOCAL);
-
-				TaskManager.addTask(new Task() {
-					
-					float time = 0;
-					
-					Entity last = teddy;
-
-					@Override
-					public boolean isFinished() {
-						return time > 3.5f;
-					}
-
-					@Override
-					public Object execute() {
-						time += Time.getDeltaTime();
-						
-						Entity teddy = World.createEntity("Teddy" + time);
-						teddy.attachComponent(MeshFilter.class).setMesh(fmesh);
-						teddy.attachComponent(MeshRenderer.class).setMaterial(fmet);
-						teddy.getTransform().setParent(last.getTransform());
-						teddy.getTransform().translate(0, Mathf.sin(1*time), 1*time, Space.LOCAL);
-						teddy.attachComponent(new Behaviour() { 
-							public void start() { } 
-							public void update() {
-								getOwner().getTransform().rotate(Mathf.degToRad(10*Time.getDeltaTime()), Vec3.UP(), Space.LOCAL);
-							} 
-						});
-						last = teddy;
-
-						return null;
-					}
-					
-				});
-				
-				return null;
-			}
-			
-		});
-		
 	}
-	
+
 	public static void main(String[] args) {
 		new Game();
 	}
